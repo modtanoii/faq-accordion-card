f75ba21adfb625fbc233a4e5ee4a36e5053a9efd
modtanoii <modnoiii@gmail.com>
2023-03-15 11:37:59 +0700

    commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
-import './Card.css';
-
-function Card(props) {
+import React from 'react'
+import './Card.css'
+function Card(props: any) {
   return <div className="card">{props.children}</div>
 }
 
